329be62fb1b397ac785251183ef8ddcee348e7c4
wettofuuu <134098789+wettofuuu@users.noreply.github.com>
2025-01-20 09:47:56 -0800

    Added delete

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -46,6 +46,63 @@ const findUserById = (id) => users["users_list"].find((user) => user["id"] === i
 
 app.use(express.json());
 
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+}
+
+function findAttributeIndex(array, attr, value){
+  for (var i = 0; i < array.length; i += 1){
+    if (array[i][attr] === value.id){
+      return i;
+    }
+  }
+  return -1;
+}
+
+const removeUser = (id) => {
+  console.log(id);
+  const index = findAttributeIndex(users["users_list"], "id", id)
+  if (index != -1){
+    users["users_list"].splice(index, 1)
+  }
+  return id;
+}
+
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
+});
+
+app.delete("/users", (req, res) => {
+  const userToRemove = req.body;
+  removeUser(userToRemove);
+  res.send();
+});
+
+
+
 app.get("/users/:id", (req, res) => {
   const id = req.params.id;
   let result = findUserById(id);
